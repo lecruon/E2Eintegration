487a648cba793f5694684ada9de6aed46c33074e
mask <mask@mask.com>
2017-12-15 15:18:15 +0100

    sdfhafdh

--- E2ESeleniumJenkins/src/testcases/TC_003.java
+++ E2ESeleniumJenkins/src/testcases/TC_003.java
@@ -3,8 +3,8 @@ package testcases;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.remote.CapabilityType;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
 
 public class TC_003 {
@@ -17,22 +17,22 @@ public class TC_003 {
 		
 		System.setProperty("webdriver.ie.driver", "./Driver/IEDriverServer.exe");
 		
-		DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
-		caps.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-		caps.setCapability(InternetExplorerDriver.ENABLE_PERSISTENT_HOVERING, false);
-		caps.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+//		DesiredCapabilities caps = DesiredCapabilities.internetExplorer();
+//		caps.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+//		caps.setCapability(InternetExplorerDriver.ENABLE_PERSISTENT_HOVERING, false);
+//		caps.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
 		
-//		InternetExplorerOptions opt = new InternetExplorerOptions();
-//		opt.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
-//		opt.setCapability(InternetExplorerDriver.ENABLE_PERSISTENT_HOVERING, false);
-//		opt.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
+		InternetExplorerOptions opt = new InternetExplorerOptions();
+		opt.setCapability(InternetExplorerDriver.INTRODUCE_FLAKINESS_BY_IGNORING_SECURITY_DOMAINS, true);
+		opt.setCapability(InternetExplorerDriver.ENABLE_PERSISTENT_HOVERING, false);
+		opt.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
 		
 //		ChromeDriver driver = new ChromeDriver();
 //		driver.findElementById("email").sendKeys("Hello");
 //		driver.findElementById("pass").sendKeys("Hello");
 		
-		driver = new InternetExplorerDriver(caps);
-//		driver.manage().window().maximize();
+		driver = new InternetExplorerDriver(opt);
+		driver.manage().window().maximize();
 
 		driver.get("http://www.facebook.com");
 		driver.findElement(By.id("email")).sendKeys("Hello");
